8a71b2d77863897e4ae269de3f14836bab9e8b74
erlinda <chambimanzanoerlinda@gmail.com>
2019-09-24 12:03:03 -0400

    variables.ts

---
+++ typeScript/variables.js
@@ -0,0 +1,6 @@
+var disableb = true;
+console.log(disableb);
+var firstname = "Erlinda";
+console.log(firstname);
+var list = [3, 7];
+console.log(list);
---
+++ typeScript/variables.ts
@@ -0,0 +1,8 @@
+let disableb:boolean=true;
+console.log(disableb);
+
+let firstname:string="Erlinda";
+console.log(firstname);
+
+let list:Array<number>=[3,7];
+console.log(list);
